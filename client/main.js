3ac51623892ce873bd1b40362cff02a353c727af
BJ Anderson <andersonb79@gmail.com>
2015-07-06 05:59:15 -0400

    initialized the angular app

---
+++ client/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('MainApp',
+  [
+    'ui.router'
+  ]
+);
+
+angular.module('MainApp').config(
+  [
+    '$urlRouterProvider',
+    function (
+      $urlRouterProvider
+    ) {
+      //configure any routes that are not controlled by ui-router
+      $urlRouterProvider
+        .otherwise('/home');  //default to the entry point for our ui-router state handlers
+    }
+  ]
+);
+
+angular.module('MainApp').controller(
+  'MainAppCtrl',
+  [
+    '$location',
+    '$scope',
+    function (
+      $location,
+      $scope
+    ) {
+
+      $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        var siteName = 'MEAN App Core';
+
+        //update the page title from the title value defined in the new state
+        if (angular.isDefined(toState.data.pageTitle)) {
+          $scope.pageTitle = toState.data.pageTitle + ' | ' + siteName;
+        } else {
+          $scope.pageTitle = siteName;
+        }
+
+      });
+    }
+  ]
+);
